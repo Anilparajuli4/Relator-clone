346b241d7d4b6e7ef28785f8b654ec7e6cee5fc1
Anil parajuli <anilparajuli580@gmail.com>
2023-07-25 18:44:19 -0700

    create pages and route

--- Relator Clone/src/App.jsx
+++ Relator Clone/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Offers from "./Pages/Offers";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ForgetPassword from "./Pages/ForgotPassword";
 function App() {
   return (
     <>
-      <h1 className="text-center">hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
