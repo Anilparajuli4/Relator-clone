5c4b959fecf5f023664e74a4ae816cfd5809e1da
Anil parajuli <anilparajuli580@gmail.com>
2023-07-27 01:50:07 -0700

    adding forgot password functionality

--- Relator Clone/src/Pages/ForgotPassword.jsx
+++ Relator Clone/src/Pages/ForgotPassword.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Oauth from "../Component/Oauth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 function ForgotPassword() {
   const [email, setEmail] = useState("");
-
+  const navigate = useNavigate();
   function onChange(e) {
     setEmail(e.target.value);
   }
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
 
+      toast.success("email was send");
+    } catch (error) {
+      toast.errot("could not send reset password");
+    }
+  }
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
@@ -21,7 +33,7 @@ function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
